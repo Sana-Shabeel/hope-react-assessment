bc33b2e13c107b7776bc9043761ee7c2fc784849
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-17 16:56:35 +0300

    Add new members

---
+++ src/component/AddMember.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+interface Props {
+  addNewMember: (name: string) => void;
+}
+
+const AddMember = ({ addNewMember }: Props) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const submitValue = () => {
+    addNewMember(inputValue);
+    setInputValue("");
+  };
+
+  return (
+    <div className="flex items-center gap-1 mt-4">
+      <input
+        className="border-2 w-4/5 h-10 rounded px-4 outline-none md:w-11/12"
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Add new member..."
+      />
+      <button
+        type="submit"
+        className="px-4 p-2 text-white bg-slate-500 rounded"
+        onClick={submitValue}
+      >
+        Add
+      </button>
+    </div>
+  );
+};
+
+export default AddMember;
