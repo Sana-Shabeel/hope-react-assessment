97211842819fc395b5c97f0e570b9f579016ad59
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-17 16:53:20 +0300

    Add search component

---
+++ src/component/Search.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+
+interface Props {
+  handleSearchMember: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const Search = ({ handleSearchMember }: Props) => {
+  return (
+    <form className="border-2 rounded relative overflow-hidden flex justify-between items-center gap-4 bg-white">
+      <input
+        onChange={(e) => handleSearchMember(e.target.value)}
+        className="w-full h-12  ml-4 outline-none"
+        type="text"
+        placeholder="Search the name..."
+      />
+      <AiOutlineSearch className="text-2xl mr-4" />
+    </form>
+  );
+};
+
+export default Search;
